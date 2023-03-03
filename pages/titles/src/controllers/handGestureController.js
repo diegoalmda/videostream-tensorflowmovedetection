4c107b9d787d7ce92e7e4e3c6c750ea542d54eff
Diego Almeida <diegoalmda@gmail.com>
2023-03-03 12:30:25 -0300

    feat: created sroll up and down

--- pages/titles/src/controllers/handGestureController.js
+++ pages/titles/src/controllers/handGestureController.js
@@ -1,7 +1,15 @@
+import { prepareRunChecker } from "../../../../lib/shared/util.js"
+
+const { shouldRun: scrollShouldRun } = prepareRunChecker({ timerDelay: 200 })
+
 export default class HandGestureController {
   #view
   #service
   #camera
+  #lastDirection = {
+    direction: '',
+    y: 0
+  }
   constructor({ view, service, camera }) {
     this.#service = service;
     this.#view = view;
@@ -10,10 +18,31 @@ export default class HandGestureController {
   async init() {
     return this.#loop()
   }
+  #scrollPage(direction) {
+    const pixelsPerScroll = 100
+    if(this.#lastDirection.direction === direction) {
+      this.#lastDirection.y = (
+        direction === 'scroll-down' ?
+        this.#lastDirection.y + pixelsPerScroll :
+        this.#lastDirection.y - pixelsPerScroll
+      )
+    }
+    else {
+      this.#lastDirection.direction = direction
+    }
+
+    this.#view.scrollPage(this.#lastDirection.y)
+  }
   async #estimateHands() {
     try {
       const hands = this.#service.estimateHands(thios.#camera.video)
-      console.log({ hands })
+      
+      for await(const { event, x, y } of this.#service.detectGestures(hands)) {
+        if(event.includes('scroll')) {
+          if(!scrollShouldRun()) continue;
+          this.#scrollPage(event)
+        }
+      }
     } catch (error) {
       console.error('Error:', error);
     }
